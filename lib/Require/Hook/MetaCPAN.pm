1d74fdd27954b66cc4cf02eb4379a8fe4798f958
perlancar (@pc-office) <perlancar@gmail.com>
2017-01-09 19:29:59 +0700

    Release v0.001

--- lib/Require/Hook/MetaCPAN.pm
+++ lib/Require/Hook/MetaCPAN.pm
@@ -10,43 +10,69 @@ use warnings;
 use HTTP::Tiny;
 
 sub new {
-    my $class = shift;
-    bless {}, $class;
+    my ($class, %args) = @_;
+    bless \%args, $class;
 }
 
-sub INC {
+sub Require::Hook::MetaCPAN::INC {
     my ($self, $filename) = @_;
 
     (my $pkg = $filename) =~ s/\.pm$//; $pkg =~ s!/!::!g;
 
     my $url = "https://metacpan.org/pod/$pkg";
     my $resp = HTTP::Tiny->new->get($url);
-    $resp->{success} or die "Can't load $filename: Can't retrieve $url: $resp->{status} - $resp->{reason}";
+    $resp->{success} or do {
+        die "Can't load $filename: Can't retrieve $url: $resp->{status} - $resp->{reason}" if $self->{die};
+        return undef;
+    };
 
-    $resp->{content} =~ m!href="(/source/[^/]+/[^/]+/[^"]*\.pm)"!
-        or die "Can't load $filename: Can't find source URL in $url";
+    $resp->{content} =~ m!href="(/source/[^/]+/[^/]+/[^"]*\.pm)"! or do {
+        die "Can't load $filename: Can't find source URL in $url" if $self->{die};
+        return undef;
+    };
 
-    $url = "https://metacpan.org$1";
+    $url = "https://fastapi.metacpan.org$1";
     $resp = HTTP::Tiny->new->get($url);
-    $resp->{success} or die "Can't load $filename: Can't retrieve $url: $resp->{status} - $resp->{reason}";
+    $resp->{success} or do {
+        die "Can't load $filename: Can't retrieve $url: $resp->{status} - $resp->{reason}" if $self->{die};
+        return undef;
+    };
 
-    eval $resp->{content};
-    die if $@;
+    \($resp->{content});
 }
 
 1;
 # ABSTRACT: Load module source code from MetaCPAN
 
+=for Pod::Coverage .+
+
 =head1 SYNOPSIS
 
  {
-     local @INC = (Require::Hook::MetaCPAN->new, @INC);
+     local @INC = (@INC, Require::Hook::MetaCPAN->new);
      require Foo::Bar; # will be searched from MetaCPAN
      # ...
  }
 
 
 =head1 DESCRIPTION
+
+
+=head1 METHODS
+
+=head2 new([ %args ]) => obj
+
+Constructor. Known arguments:
+
+=over
+
+=item * die => bool (default: 1)
+
+If set to 1 (the default) will die if module source code can't be fetched (e.g.
+the module does not exist on CPAN, or there is network error). If set to 0, will
+simply decline so C<require()> will try the next entry in C<@INC>.
+
+=back
 
 
 =head1 SEE ALSO
