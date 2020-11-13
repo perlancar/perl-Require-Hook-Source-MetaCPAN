491ae21e717f6726c567cdfffd065beb96a3c196
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-11-13 09:47:32 +0700

    Reorganize namespace, add category: RH:MetaCPAN -> RH:Source::MetaCPAN; update to make it work again

--- lib/Require/Hook/Source/MetaCPAN.pm
+++ lib/Require/Hook/Source/MetaCPAN.pm
@@ -1,20 +1,29 @@
-package Require::Hook::MetaCPAN;
+package Require::Hook::Source::MetaCPAN;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 use 5.010001;
 use strict;
 use warnings;
+use Log::ger;
 
+# preload to avoid deep recursion in our INC
 use HTTP::Tiny;
+use IO::Socket::SSL;
+use URI::URL;
+# to trigger lazy loading
+{ my $url = URI::URL->new("/foo", "https://example.com")->abs }
 
 sub new {
     my ($class, %args) = @_;
+    $args{die} = 1 unless defined $args{die};
     bless \%args, $class;
 }
 
-sub Require::Hook::MetaCPAN::INC {
+sub Require::Hook::Source::MetaCPAN::INC {
     my ($self, $filename) = @_;
 
     (my $pkg = $filename) =~ s/\.pm$//; $pkg =~ s!/!::!g;
@@ -26,12 +35,13 @@ sub Require::Hook::MetaCPAN::INC {
         return undef;
     };
 
-    $resp->{content} =~ m!href="(/source/[^/]+/[^/]+/[^"]*\.pm)"! or do {
+    $resp->{content} =~ m!href="(.+?\?raw=1)"! or do {
         die "Can't load $filename: Can't find source URL in $url" if $self->{die};
         return undef;
     };
 
-    $url = "https://fastapi.metacpan.org$1";
+    $url = URI::URL->new($1, $url)->abs . "";
+    log_trace "[RH:Source::MetaCPAN] Retrieving module source for $filename from $url ...";
     $resp = HTTP::Tiny->new->get($url);
     $resp->{success} or do {
         die "Can't load $filename: Can't retrieve $url: $resp->{status} - $resp->{reason}" if $self->{die};
@@ -49,13 +59,16 @@ sub Require::Hook::MetaCPAN::INC {
 =head1 SYNOPSIS
 
  {
-     local @INC = (@INC, Require::Hook::MetaCPAN->new);
+     local @INC = (@INC, Require::Hook::Source::MetaCPAN->new);
      require Foo::Bar; # will be searched from MetaCPAN
      # ...
  }
 
 
 =head1 DESCRIPTION
+
+Warning: this is most probably not suitable for use in production or real-world
+code.
 
 
 =head1 METHODS
@@ -66,7 +79,9 @@ Constructor. Known arguments:
 
 =over
 
-=item * die => bool (default: 1)
+=item * die
+
+Bool. Default is true.
 
 If set to 1 (the default) will die if module source code can't be fetched (e.g.
 the module does not exist on CPAN, or there is network error). If set to 0, will
@@ -78,3 +93,5 @@ simply decline so C<require()> will try the next entry in C<@INC>.
 =head1 SEE ALSO
 
 Other C<Require::Hook::*> modules.
+
+L<Require::HookChain::source::metacpan> uses us.
